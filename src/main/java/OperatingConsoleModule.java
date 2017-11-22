d333ac15c95899766dd6f000a968b97aaa51909d
P44elovod <eug.kovtun@gmail.com>
2017-11-23 01:27:36 +0200

    обработка ввдо из консоли

---
+++ src/main/java/OperatingConsoleModule.java
@@ -0,0 +1,33 @@
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+@Getter
+public class OperatingConsoleModule {
+
+    private static List<String> pidList;
+
+
+
+
+public static List consoleRead(){
+
+        Scanner scanner = new Scanner(System.in);
+
+    return pidToDestroy(scanner.nextLine());
+
+
+}
+
+
+    private static List pidToDestroy(String listFromConsole){
+
+        return pidList = Arrays.stream(listFromConsole.split(",")).collect(Collectors.toList());
+
+    }
+
+
+}
