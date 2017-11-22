f127484237165d7449d6453c8ce503b00d47c959
P44elovod <eug.kovtun@gmail.com>
2017-11-23 00:54:01 +0200

    изменения в организации класса

--- src/main/java/ProcessPanelModule.java
+++ src/main/java/ProcessPanelModule.java
@@ -1,34 +1,44 @@
 import java.util.Comparator;
 
 
-public class ProcessReader {
+public class ProcessPanelModule {
 
 
     public static void main(String[] args) {
         ProcessHandle.allProcesses()
                 .filter(ph -> ph.info().command().isPresent())
                 .limit(10)
-                .sorted(Comparator.comparing(ProcessReader::cpuDuration)
+                .sorted(Comparator.comparing(ProcessPanelModule::cpuDuration)
                         .reversed()
                 )
                 .forEach(ph -> {
                             System.out.printf("%d ", ph.pid());
-                            System.out.printf("%s ", ph.info().command().orElse(""));
-                            System.out.printf("[%sms]",
-                                    ph.info().totalCpuDuration()
-                                            .get().toMillis());
+                            System.out.printf("%s ", dumpCommand(ph));
+                            System.out.printf("[%sms]", cpuDuration(ph));
                             System.out.println();
                         }
                 );
     }
 
-    static Long cpuDuration(ProcessHandle ph) {
+ private static Long cpuDuration(ProcessHandle ph) {
         return ph
                 .info()
                 .totalCpuDuration()
                 .get()
                 .toMillis();
     }
+
+    private static String dumpCommand(ProcessHandle ph){
+        return ph.info()
+                .command()
+                .orElse("");
+    }
+
+
+
+
+
+
 
 
 }
